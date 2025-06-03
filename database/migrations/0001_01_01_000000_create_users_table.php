612135174b8ce3768282d06f479e2b09235a87e8
aimanyusufw <aimanyusufdev@gmail.com>
2025-06-04 01:20:49 +0700

    ref : set phone and address to nullabel or optional input

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone')->unique();
-            $table->string('address')->unique();
+            $table->string('phone')->unique()->nullable();
+            $table->string('address')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
